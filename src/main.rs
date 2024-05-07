9c0d3fe37c63478368f328c1d6fa753d30691e90
Kendall Auel <kendalla59@gmail.com>
2024-05-07 07:05:32 -0700

    Checkpoint

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+// main.rs
+//
+// Author: Kendall Auel
+// Description:
+//     Entry point for the railroad signaling case study implementation.
+//
+
+const SG_VERSION_MAJOR: u32 = 1;
+const SG_VERSION_MINOR: u32 = 0;
+
+// ------------------------------------------------------------------
+// main -- Entry point
+// ------------------------------------------------------------------
+
+fn main() {
+    println!("Case Study Implementation -- Railroad Signaling System");
+    println!("Version {SG_VERSION_MAJOR}.{SG_VERSION_MINOR}");
+}
+//    while (runCommand() == 0) {}
+//}
+
+//    sys().resetTrackNetwork();
+//    return 0;
+//}
